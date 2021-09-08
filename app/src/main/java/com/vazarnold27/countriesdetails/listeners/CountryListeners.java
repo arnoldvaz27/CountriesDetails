d6eca3c0823ae727a4a7ed7610f37f7bd99b7a88
arnoldvaz27 <arnoldvaz27.github@gmail.com>
2021-09-08 17:59:23 +0530

    Countries Details Project added to Git.

---
+++ app/src/main/java/com/vazarnold27/countriesdetails/listeners/CountryListeners.java
@@ -0,0 +1,9 @@
+package com.vazarnold27.countriesdetails.listeners;
+
+import com.vazarnold27.countriesdetails.entites.Country;
+
+public interface CountryListeners {
+
+    void onItemClicked(Country country, int position); //event listener after clicking on the view
+
+}
